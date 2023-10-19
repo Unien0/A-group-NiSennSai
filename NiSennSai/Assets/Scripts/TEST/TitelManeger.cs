4bd53fb27326e698fc827eee3f89c39780fca5d0
Rui <22cr0110@jec.ac.jp>
2023-10-19 23:40:38 +0900

    タイトルシーンの準備

---
+++ NiSennSai/Assets/Scripts/TEST/TitelManeger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitelManeger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ボタン入力に応じてシーン遷移
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
